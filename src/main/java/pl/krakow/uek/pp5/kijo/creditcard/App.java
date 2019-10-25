2f88cb76d8c7c61a215306fb084bd9e59f4beb7c
kijo <kamil31@interia.eu>
2019-10-25 02:45:22 +0200

    testing adding from laptop

--- src/main/java/pl/krakow/uek/pp5/kijo/creditcard/App.java
+++ src/main/java/pl/krakow/uek/pp5/kijo/creditcard/App.java
@@ -2,6 +2,8 @@ package pl.krakow.uek.pp5.kijo.creditcard;
 
 public class App {
     public static void main(String[] args) {
+
         System.out.println("elo");
+        System.out.println(" no");
     }
 }
