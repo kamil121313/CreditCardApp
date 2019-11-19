78bc50bbe4af4434da89704ca906c321c1405d60
kijo <kamil31@interia.eu>
2019-11-20 00:30:58 +0100

    test

--- src/main/java/pl/krakow/uek/pp5/kijo/creditcard/App.java
+++ src/main/java/pl/krakow/uek/pp5/kijo/creditcard/App.java
@@ -3,7 +3,7 @@ package pl.krakow.uek.pp5.kijo.creditcard;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("elo");
+        System.out.println("test");
         System.out.println(" no");
     }
 }
